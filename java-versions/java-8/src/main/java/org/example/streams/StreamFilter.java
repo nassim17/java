894e1498e16846ca492aff0948270f4e692e3999
nabed <abednassim92@gmail.com>
2023-11-06 16:57:00 +0100

    Java-8 - StreamFilter - Example 1

--- java-versions/java-8/src/main/java/org/example/streams/StreamFilter.java
+++ java-versions/java-8/src/main/java/org/example/streams/StreamFilter.java
@@ -1,8 +1,13 @@
 package org.example.streams;
 
+import java.util.stream.Stream;
+
 public class StreamFilter {
 
   public static void main(String[] args){
 
+    Stream<String> myStream = Stream.of("Tom", "Ben", "Artur", "John");
+    myStream.filter(x -> x.startsWith("A"))
+        .forEach(System.out::println);
   }
 }
