72a522f43a9e4c79ed42cc3bad19e904241ba24f
nabed <abednassim92@gmail.com>
2023-11-06 17:00:32 +0100

    Java-8 - StreamFilter - Integer Stream

--- java-versions/java-8/src/main/java/org/example/streams/StreamFilter.java
+++ java-versions/java-8/src/main/java/org/example/streams/StreamFilter.java
@@ -9,5 +9,10 @@ public class StreamFilter {
     Stream<String> myStream = Stream.of("Tom", "Ben", "Artur", "John");
     myStream.filter(x -> x.startsWith("A"))
         .forEach(System.out::println);
+
+    Integer[] myArray = new Integer[] { 1, 4, 5, 7, 9, 10 };
+    Stream.of(myArray)
+        .filter(x -> x % 2 == 0)
+        .forEach(System.out::println);
   }
 }
